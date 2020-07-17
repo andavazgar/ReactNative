be29f6ff41f50bd1c4115ab1d02ba95cbe122523
Andrés Vazquez <andavazgar.dev@gmail.com>
2020-07-17 16:35:07 -0400

    Added a check to reduce the number of UIFaces fetches

--- DesignCode-app/app/components/Avatar.tsx
+++ DesignCode-app/app/components/Avatar.tsx
@@ -1,26 +1,30 @@
-import React, { FC, useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components/native";
 
+import { RootState } from "../store/configureStore";
 import { updateUser } from "../store/user";
 
 export interface AvatarProps {}
 
-const Avatar: FC<AvatarProps> = ({}) => {
+const Avatar: React.FC<AvatarProps> = ({}) => {
   const [avatar, setAvatar] = useState("https://cl.ly/55da82beb939/download/avatar-default.jpg");
+  const user = useSelector((state: RootState) => state.user);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    fetch("https://uifaces.co/api?limit=1", {
-      headers: new Headers({
-        "X-API-KEY": "eeaafbe81657073cd70ac6e3de1bd6",
-      }),
-    })
-      .then((response) => response.json())
-      .then((jsonResponse) => {
-        dispatch(updateUser(jsonResponse[0]));
-        setAvatar(jsonResponse[0].photo);
-      });
+    if (Object.keys(user).length === 0) {
+      fetch("https://uifaces.co/api?limit=1", {
+        headers: new Headers({
+          "X-API-KEY": "eeaafbe81657073cd70ac6e3de1bd6",
+        }),
+      })
+        .then((response) => response.json())
+        .then((jsonResponse) => {
+          dispatch(updateUser(jsonResponse[0]));
+          setAvatar(jsonResponse[0].photo);
+        });
+    }
   }, []);
 
   return <Image source={{ uri: avatar }} />;
